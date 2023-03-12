9a615b00d7a6f481bde4cef31be7dc63e9480773
Анастасия Каплуновская <nastya.anna2013@yandex.ru>
2023-03-12 18:27:10 +0300

    Added all changes after review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import get_object_or_404
-
-from rest_framework import permissions
-
-from rest_framework import viewsets
-
 from posts.models import Group, Post
-
+from .permissions import IsAuthorOrReadOnly
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .serializers import (
     CommentSerializer, GroupSerializer, PostSerializer
 )
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    message = 'Изменение чужого контента запрещено!'
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -28,7 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -37,7 +26,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [
-        permissions.IsAuthenticated, IsAuthorOrReadOnly
+        IsAuthenticated, IsAuthorOrReadOnly
     ]
 
     def get_queryset(self):
